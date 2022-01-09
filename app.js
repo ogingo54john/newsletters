d88f8d93c6565a08aab6cb83fb7ec7a56950b98c
unknown <ogingojohn54@gmail.com>
2022-01-09 09:24:01 +0300

    modified app and signup pages

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const request = require('request')
+//const request = require('request')
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
 app.get('/',function(req,res){
     res.sendFile(__dirname +"/signup.html")
 })
@@ -11,5 +13,16 @@ app.listen(3000, function(){
 })
 
 app.post('/', function(req,res){
-console.log(req.body.firstName)
+var firstName = req.body.firstName;
+var lastName = req.body.lastName;
+var email = req.body.email; 
+
+console.log(firstName, lastName,email)
+if(res.statusCode==200){
+    res.send("successfully subscribed");
+}else{
+    res.send("There was an error can't suscibe");
+}
+
 })
+//238b869dd27d4c46399aafd68c8b93cf-us20
